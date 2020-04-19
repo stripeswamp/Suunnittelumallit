d6444efa0b75130ae085a41041b5d15e82bbecf5
Administrator <Administrator@WIN-DP3A01G7US3>
2020-04-19 16:46:26 +0300

    Tehtävä 04

---
+++ Tehtävä04/src/ClockTimer.java
@@ -0,0 +1,43 @@
+
+import java.util.Observable;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Administrator
+ */
+public class ClockTimer extends Observable implements Runnable {
+    
+    
+    
+    public int getHour()
+    {
+        return 0;
+    }
+    
+    public int getMinute()
+    {
+        return 0;
+    }
+    
+    public int getSecond()
+    {
+        return 0;
+    }
+    
+    void tick()
+    {
+        
+    }
+    
+    @Override
+    public void run()
+    {
+        
+    }
+}
---
+++ Tehtävä04/src/DigitalClock.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Administrator
+ */
+public class DigitalClock extends ClockTimer {
+    
+    private ClockTimer timer;
+    
+    public DigitalClock(ClockTimer clockTimer)
+    {
+        this.timer = clockTimer;
+        
+    }
+}
