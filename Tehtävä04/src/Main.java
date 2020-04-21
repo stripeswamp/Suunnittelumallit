64fe09238237ee37c2e778095c15ae9b785b073c
Administrator <Administrator@WIN-DP3A01G7US3>
2020-04-21 12:17:59 +0300

    Revert "Tehtävä 14"
    This reverts commit 8b535f0c481224d7adcf2047173e1f2451818bce.

--- Tehtävä04/src/Main.java
+++ Tehtävä04/src/Main.java
@@ -8,9 +8,13 @@
  *
  * @author Administrator
  */
-abstract class Hambuilder {
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
     
-    public abstract Object getBurger();
-    public abstract void initiateBurger();
-    public abstract void addToBurger();
 }
