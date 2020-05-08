9b0fe531777ed293b36da1f357d591fd95884cb3
stripeswamp <Administrator@WIN-DP3A01G7US3>
2020-05-08 15:08:13 +0300

    Tehtävä 09

---
+++ Tehtävä09/src/BubbleSort.java
@@ -0,0 +1,28 @@
+public class BubbleSort implements SortStrategy {
+    
+    @Override
+    public void sort(int[] array)
+    {
+        int n = array.length;
+        int k;
+        for (int m = n; m >= 0; m--) 
+        {
+            for (int i = 0; i < n - 1; i++) 
+            {
+                k = i + 1;
+                if (array[i] > array[k]) 
+                {
+                    swapNumbers(i, k, array);
+                }
+            }
+        }
+    }
+
+    private void swapNumbers(int i, int j, int[] array)
+    {
+        int temp;
+        temp = array[i];
+        array[i] = array[j];
+        array[j] = temp;
+    }
+}
